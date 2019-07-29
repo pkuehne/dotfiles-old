10135db856297d9d13979b5033e6fb9485c44a36
Peter Kuehne <peter.kuehne@gmail.com>
2019-07-29 15:11:08 +0100

    Run ansible as part of install

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
+# Install ansible
 ansible_path="$(which ansible)"
 if [ "$ansible_path" != "" ]
 then
@@ -8,6 +9,7 @@ else
     sudo ./scripts/setup_ansible.sh
 fi
 
+# Create vars.yml file
 vars_file="./vars.yml"
 if [ -f "$vars_file" ]
 then
@@ -15,5 +17,8 @@ then
 else
     ./scripts/setup_vars.sh 
 fi
+
+# Run ansible
+ansible-playbook playbook.yml
 
 exit 0
