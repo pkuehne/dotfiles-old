948fffd5d598d57b3e9da9bd501303ebf7730343
Peter Kuehne <peter.kuehne@gmail.com>
2019-07-29 11:12:30 +0100

    Ensure install is run with sudo and tidy order

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #! /usr/bin/env bash
 
-sudo apt update
-sudo apt install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt update
-sudo apt install -y ansible
+if [ "$EUID" -ne 0 ]
+then
+    echo "Please re-run as root"
+    exit 1
+fi
+
+apt-add-repository -y ppa:ansible/ansible
+apt update
+apt install -y software-properties-common
+apt install -y ansible
